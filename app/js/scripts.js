872ad5cd5d0ea2c1c2bfeb4291c5293732d76326
Miguel Angel Fernandez <elmendalerenda@gmail.com>
2017-04-29 18:13:54 +0200

    add iss datasource and switch camera to fullscreen

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,17 +1,7 @@
-var mapElement = $('#map');
-var dataSources = $('#datasource');
-
-dataSources.on('change', function(){
-  switch(parseInt(this.value)) {
-    case 0:
-      selectLandslide();
-      break;
-  }
-});
-
 initialize();
 
 function stream(){
+  console.log('streaming camera...');
   navigator.mediaDevices.getUserMedia({
     'audio': false,
     'video': true
@@ -22,39 +12,73 @@ function stream(){
   }).catch(function() {
         alert('Unable to access media');
   });
+	$('#camera').addClass('video_background');
+  $('#map').hide();
+}
+
+function selectISSLive(){
+    console.log('starting ISS live');
+    httpGet("https://api.wheretheiss.at/v1/satellites/25544",
+        function(issLocation){
+          goTo(issLocation.latitude, issLocation.longitude);
+          setTimeout(stream, 1000);
+    // setInterval(selectISSLive, 5000);
+        });
 }
 
 function selectLandslide(){
-  var json_landslides = JSON.parse(httpGet("https://data.nasa.gov/resource/tfkf-kniw.json"));
-  var locationElement = $('#location');
-  locationElement.show();
-  var infoElement = $('#info');
-  for(var i=0; i<5; i++) {
-    var loc = json_landslides[i].adminname1;
-    locationElement.append("<option value='" + i + "'>" + loc + "</option>");
-  }
+  console.log('starting landslides...');
+  httpGet("https://data.nasa.gov/resource/tfkf-kniw.json",
+      function(landslides) {
+        var locationElement = $('#location');
+        locationElement.show();
+        var infoElement = $('#info');
+        for(var i=0; i<5; i++) {
+          var loc = landslides[i].adminname1;
+          locationElement.append("<option value='" + i + "'>" + loc + "</option>");
+        }
 
-  locationElement.on('change', function(){
-    var selectedLandslide = json_landslides[this.value];
-    infoElement.text("Country: " + selectedLandslide.countryname + ", date: "+ selectedLandslide.date );
-    var bbox = getBBox(parseFloat(selectedLandslide.latitude), parseFloat(selectedLandslide.longitude), 100000);
-    var url = 'http://www.openstreetmap.org/export/embed.html?bbox=' + bbox[0] + '%2C' + bbox[1] + '%2C' + bbox[2] + '%2C' + bbox[3] +  '&amp;layer=mapnik&amp;marker=' + selectedLandslide.latitude + '%2C' + selectedLandslide.longitude;
-    mapElement.attr('src', url);
+        locationElement.on('change', function(){
+          var selectedLandslide = landslides[this.value];
+          infoElement.text("Country: " + selectedLandslide.countryname + ", date: "+ selectedLandslide.date );
+          goTo(selectedLandslide.latitude, selectedLandslide.longitude);
+        });
   });
 }
 
 function initialize(){
-  $('#datasource').val(-1);
-  $('#start').on('click', function(e) { stream(); });
-jQuery('#overlay').width(jQuery('#camera').width()).height(jQuery('#camera').height()).css({top:jQuery('#camera').offset().top,left:jQuery('#camera').offset().left});
+  var dataSources = $('#datasource');
+  dataSources.val(-1);
+
+  dataSources.on('change', function(){
+    switch(parseInt(this.value)) {
+      case 0:
+        selectLandslide();
+        break;
+      case 1:
+        selectISSLive();
+        break;
+    }
+  });
+
+  jQuery('#overlay').width(jQuery('#camera').width()).height(jQuery('#camera').height()).css({top:jQuery('#camera').offset().top,left:jQuery('#camera').offset().left});
 }
 
-function httpGet(theUrl)
+function httpGet(theUrl, success)
 {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
+  $.get( theUrl, success).fail(function() {
+    console.log( "error calling " + theUrl );
+  });
+  // var xmlHttp = new XMLHttpRequest();
+  // xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  // xmlHttp.send( null );
+  // return xmlHttp.responseText;
+}
+
+function goTo(lat, lon){
+    var bbox = getBBox(parseFloat(lat), parseFloat(lon), 100000);
+    var url = 'http://www.openstreetmap.org/export/embed.html?bbox=' + bbox[0] + '%2C' + bbox[1] + '%2C' + bbox[2] + '%2C' + bbox[3] +  '&amp;layer=mapnik&amp;marker=' + lat + '%2C' + lon;
+    $('#map').attr('src', url);
 }
 
 function getCoordOffset(what, lat, lon, offset) {
